6582007c6bcc109d7f14080a6a8f763ac4a31d65
kaayush163 <kaayush163>
2023-11-26 17:55:25 +0530

    chatbot

---
+++ config/pinecone.ts
@@ -0,0 +1,18 @@
+/**
+ * Change the namespace to the namespace on Pinecone you'd like to store your embeddings.
+ */
+
+if (!process.env.PINECONE_INDEX_NAME) {
+  throw new Error('Missing Pinecone index name in .env file');
+}
+
+const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
+
+// const PINECONE_NAME_SPACE = 'pdf-test'; //namespace is optional for your vectors
+
+const namespace = 'pdf-test'; //namespace is optional for your vectors
+// const namespace = 'gpt4-pdf-langchain'; //namespace is optional for your vectors
+
+// export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
+export { PINECONE_INDEX_NAME, namespace };
+
---
+++ scripts/ingest-data.ts
@@ -0,0 +1,63 @@
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { pinecone } from '@/utils/pinecone-client';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+// import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
+import { PINECONE_INDEX_NAME, namespace } from '@/config/pinecone';
+
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+
+
+// const filePath = express.static(path.join(__dirname,"../docs/MorseVsFrederick.pdf"));
+
+/* Name of directory to retrieve your files from 
+   Make sure to add your PDF files inside the 'docs' folder
+*/
+// const filePath = "docs/MorseVsFrederick.pdf"
+const filePath = "docs/"
+
+// const filePath = path.join(__dirname, '../docs/MorseVsFrederick.pdf');
+
+export const run = async () => {
+  try {
+    /*load raw docs from the all files in the directory */
+    const directoryLoader = new DirectoryLoader(filePath, {
+      '.pdf': (path) => new PDFLoader(path),
+    });
+
+    // const loader = new PDFLoader(filePath);
+    const rawDocs = await directoryLoader.load();
+
+    /* Split text into chunks */
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const docs = await textSplitter.splitDocuments(rawDocs);
+    console.log('split docs', docs);
+
+    console.log('creating vector store...');
+    /*create and store the embeddings in the vectorStore*/
+    const embeddings = new OpenAIEmbeddings();
+    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+
+    //embed the PDF documents
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      // namespace: PINECONE_NAME_SPACE,
+      namespace: namespace,
+
+      textKey: 'text',
+    });
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to ingest your data');
+  }
+};
+
+(async () => {
+  await run();
+  console.log('ingestion complete');
+})();
